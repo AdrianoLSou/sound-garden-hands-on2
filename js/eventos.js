e069b1c6aab89e3235b3dc85c66683a3028fdade
Adriano Leal <adrianoleal35@gmail.com>
2022-04-28 23:19:33 -0300

    ajuste de tabela

--- js/eventos.js
+++ js/eventos.js
@@ -2,41 +2,39 @@ let eventoID;
 
 function exibirEventos(eventos) {
     eventos.forEach(evento => {
-        //Clona o modelo de elemento HTML do evento para ser edidato mais abaixo
+        
         const eventoModel = document.querySelector('.models article').cloneNode(true);
         
-        //Formata a data pro padrao local dd/mm/yyyy
+        
         const data = new Date(evento.scheduled);
         const dataFormatada = data.toLocaleDateString();
 
-        //Titulo e Data
+        
         eventoModel.querySelector('h2').innerHTML = `${evento.name} - ${dataFormatada}`;
-        //Atracoes
+        
         eventoModel.querySelector('h4').innerHTML = evento.attractions.join(', ');
-        //Descricao
+       
         eventoModel.querySelector('p').innerText = evento.description;
-        //Adiciona um evento de click em cada botão reservar ingresso
+        
         eventoModel.querySelector('a').addEventListener('click', (e) => {
             e.preventDefault();
-            //Seleciona o modal e atribui a variavel modal pra ele
+            
             const modal = document.querySelector('.modal');
             
-            //Muda o display do modal que está 'none' pra 'flex'
+            
             modal.style.display = 'flex';
-            //Adiciona no titulo o nome + data do evento
+           
             document.querySelector('.modal-window p').innerHTML = `<b>Evento</b>: ${evento.name}<br><b>Data</b>: ${dataFormatada}`
             
-            //Adicionando função do botão de fechar modal
+            
             document.querySelector('.modal-window span').addEventListener('click', () => {
                 modal.style.display = 'none';
             })
 
-            //Seta o id do evento atual acessado ao clicar no no botao de reservar
+            
             eventoID = evento._id
         })
-        //Apos preparar nosso elemento HTML com as informacoes desse evento
-        //Entao usamos um append para adiciona-lo na pagina
-        //No HTML peguei a Div que guarda os elementos e dei um id 'boxEventos' para ela
+       
         document.getElementById('boxEventos').append(eventoModel);
     });
 }
@@ -51,39 +49,38 @@ try {
 }
 
 
-//Função do botão reservar do modal
+
 document.querySelector('.modal-window a').addEventListener('click', (e) => {
     e.preventDefault();
 
     const modal = document.querySelector('.modal');
-    //Seleciona os inputs/campos do modal e atribui uma variavel a eles
+    
     let nome = document.querySelector(`.modal-window #nomeInput`);
     let email = document.querySelector(`.modal-window #emailInput`);
     let qntTickets = document.querySelector(`.modal-window label > input`);
     
-    //Body da requisicao
+    
     let body = {
         owner_name: nome.value,
         owner_email: email.value,
         number_tickets: qntTickets.value,
         event_id: eventoID
     }
-    //Detalhes da requisicao
+    
     const requisicao = {
         method: 'POST',
-        body: JSON.stringify(body), //Aqui é passamos o body e convertemos em string
+        body: JSON.stringify(body), 
         headers: {"Content-type": "application/json"}
     }
-    //Faz a uma requisicao POST para reservar o(s) ticket(s)
+    
     fetch("https://xp41-soundgarden-api.herokuapp.com/bookings", requisicao)
         .then(response => {
-            //Emite um erro se retornar algum statuscode de erro
-            //Ao emitir o erro, ele interrompe o .then e cai no .catch
+           
             if(response.status != 201) {
                 throw new Error();
             }
 
-            modal.style.display = 'none'; //Fecha o modal
+            modal.style.display = 'none'; 
             nome.value = "";
             email.value = "";
             qntTickets.value = 1;
@@ -91,7 +88,7 @@ document.querySelector('.modal-window a').addEventListener('click', (e) => {
             alert('Reservado com sucesso!');
         })
         .catch(error => {
-            //Mostra um alerta informando erro e printa no console o erro
+            
             alert('Algo saiu errado, tente novamente :C');
             console.error('error: ', error.message);
         })
--- js/index.js
+++ js/index.js
@@ -15,26 +15,24 @@ function exibirEventos(listaEventos) {
     const eventosOrdenados = orderEvents(listaEventos);
     const eventos = removePastEvents(eventosOrdenados);
 
-    //Seleciona os 3 eventos da pagina index.html
+    
     const divEventos = document.getElementById("divEventos").children;
         
-    //Faz um For nos 3 eventos da pagina index.html
+    
     for(let i=0; i < divEventos.length; i++) {
-        const item = divEventos[i]; //Cada evento da pagina inicial, Total: 3
-        const evento = eventos[i]; //Cada evento da lista de eventos que veio da API
+        const item = divEventos[i]; 
+        const evento = eventos[i]; 
         
-        //Cria e converte a data para o formato local dd/mm/yyyy
         const data = new Date(evento.scheduled);
         const dataFormatada = data.toLocaleDateString();
         
-        //Titulo e Data
+       
         item.querySelector('h2').innerText = `${evento.name} - ${dataFormatada}`;
-        //Atracoes
+       
         item.querySelector('H4').innerHTML = evento.attractions.join(', ');
-        //Descricao
+        
         item.querySelector('p').innerText = evento.description
 
-        //Adiciona um evento de click no botão reservar ingresso
         item.querySelector('a').addEventListener('click', (e) => {
             e.preventDefault();
 
@@ -43,12 +41,10 @@ function exibirEventos(listaEventos) {
             modal.style.display = 'flex';
             document.querySelector('.modal-window p').innerHTML = `<b>Evento</b>: ${evento.name}<br><b>Data</b>: ${dataFormatada}`
             
-            //Adicionando função do botão de fechar modal
             document.querySelector('.modal-window span').addEventListener('click', () => {
                 modal.style.display = 'none';
             })
             
-            //Seta o id do evento atual acessado ao clicar no no botao de reservar
             eventoID = evento._id
         })
 
@@ -66,7 +62,7 @@ try {
 }
 
 
-//Função do botão reservar do modal
+
 document.querySelector('.modal-window a').addEventListener('click', (e) => {
     e.preventDefault();
     const modal = document.querySelector('.modal');
@@ -75,31 +71,30 @@ document.querySelector('.modal-window a').addEventListener('click', (e) => {
     let email = document.querySelector(`.modal-window #emailInput`);
     let qntTickets = document.querySelector(`.modal-window label > input`);
     
-    //Body da requisicao
+
     const body = {
         owner_name: nome.value,
         owner_email: email.value,
         number_tickets: qntTickets.value,
         event_id: eventoID
     }
-    //Detalhes da requisicao
+    
     const requisicao = {
         method: 'POST',
         body: JSON.stringify(body),
         headers: {"Content-type": "application/json"}
     }
-    //Faz a uma requisicao POST para reservar o(s) ticket(s)
+    
     fetch("https://xp41-soundgarden-api.herokuapp.com/bookings", requisicao)
         .then(response => {
-            //Emite um error se retornar algum statuscode de erro
-            //Envia o error para o catch
+           
             if(response.status != 201) {
                 throw new Error();
             }
 
             alert('Reservado com sucesso!');
 
-            //Fecha o modal
+            
             modal.style.display = 'none';
             nome.value = "";
             email.value = "";
